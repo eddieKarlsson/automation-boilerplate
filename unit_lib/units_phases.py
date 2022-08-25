159b2c5b0098dafd6b85cd8d24ce763d077da025
eddieKarlsson <eddieperty@gmail.com>
2022-08-25 10:52:50 +0200

    added combination of intouch files

--- unit_lib/units_phases.py
+++ unit_lib/units_phases.py
@@ -1,6 +1,9 @@
 import os
 import os.path
 from gen_unit_func import GenUnitFunc as genfunc
+from os import listdir
+from os.path import isfile, join
+
 
 
 class UnitsPhases:
@@ -22,6 +25,8 @@ class UnitsPhases:
         self.gen = genfunc(gen_main)
 
         self.rl = []  # Create empty list "result list"
+        
+        self.it_all_all_path_list = []
 
         # Check if list is empty, if it is print an error
         if self.ol:
@@ -66,6 +71,7 @@ class UnitsPhases:
 
 
     def _create_intouch_file(self, path, obj):
+        """Creates intouch file and adds path to file to dictionary"""
         config_file = os.path.join(self.cp, obj['type'] + '.txt')
         data = self.gen.single_obj(config_file, self.rl, obj, obj['type'] + '_IT')
 
@@ -76,8 +82,55 @@ class UnitsPhases:
         path = os.path.join(path, filename)
         with open(path, 'w', encoding='cp1252') as f:
             f.write(data)
+        
+        if obj['parent'] is None and obj['is_unit']:
+            obj['it_file_path'] = path  #Create new property, intouch file path
+
+
+    def combine_intouch_files_same_unit(self):
+        """Combines intouch files and writes a "ALL" file to the group folder"""
+        for obj in self.ol:
+            if obj['parent'] is None and obj['is_unit']:
+                folder_path, fname = os.path.split(obj['it_file_path'])
+                all_filename = f"ALL_{obj['id']}.csv"
+                all_path = os.path.join(folder_path, all_filename)
+            
+                if os.path.exists(all_path):
+                    os.remove(all_path)
+
+                file_list = [f for f in listdir(folder_path)
+                    if isfile(join(folder_path, f))]
+
+                with open(all_path, 'w', encoding='cp1252') as wf:
+                    for file_index, file in enumerate(file_list):
+                        with open(os.path.join(folder_path, file), 'r', encoding='cp1252') as rf:
+                            for line_index, line in enumerate(rf):
+                                # Skip first line header if it's not the first file
+                                if file_index > 0 and line_index <= 0:
+                                    continue
+                                wf.write(line)
+                self.it_all_all_path_list.append(all_path)
+
+
+    def combine_intouch_all_all_files(self):
+        """Combines intouch files and writes a "ALL" file to the group folder"""
+        all_filename = f"ALL_{self.type.upper()}_IT.csv"
+        all_path = os.path.join(self.it_path, all_filename)
+            
+        with open(all_path, 'w', encoding='cp1252') as wf:
+            for file_index, file in enumerate(self.it_all_all_path_list):
+                with open(file, 'r', encoding='cp1252') as rf:
+                    for line_index, line in enumerate(rf):
+                        # Skip first line header if it's not the first file
+                        if file_index > 0 and line_index <= 0:
+                            continue
+                        wf.write(line)
+         
+
 
 
     def generate(self):
         self.intouch()
         self.gen.result(self.rl, type=self.type.upper())
+        self.combine_intouch_files_same_unit()
+        self.combine_intouch_all_all_files()
