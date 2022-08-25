a7c7db0f5060b4be80ea5cd3aa077bf52cf8b52d
eddieKarlsson <eddieperty@gmail.com>
2022-08-25 12:38:22 +0200

    upd version number

--- settings.py
+++ settings.py
@@ -7,7 +7,7 @@ class Settings:
     """A class to store all settings, user-data in JSON format."""
 
     def __init__(self):
-        self.version = 2.2
+        self.version = 2.3
 
         """Settings"""
         self.HEADER_ROW = 3  # Excel header
