d41445f7566ecb823931e1065339f4652ca7f542
eddieKarlsson <eddieperty@gmail.com>
2023-08-11 10:57:32 +0200

    typo

--- unit_lib/unit_types.py
+++ unit_lib/unit_types.py
@@ -1,11 +1,12 @@
 from enum import Enum, auto
 
+
 class UnitTypes(Enum):
     """Class to represent the different types of units / phases, must correspond to
     the table in excel under _configs"""
-    
+
     PHASE = 'Phase'
     LINE_UNIT = 'Line_Unit'
     TANK_UNIT = 'Tank_Unit'
-    LINE_SROUCE = 'Line_Source'
+    LINE_SOURCE = 'Line_Source'
     LINE_DESTINATION = 'Line_Destination'
